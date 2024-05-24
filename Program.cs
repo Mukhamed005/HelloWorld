1962b252068332089e413703d67161441e8b8d4d
Olezha1852 <muxasultanbekov00@gmail.com>
2024-05-24 15:06:33 +0300

    COMMIT_EDITMSG

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите свое имя");
+        string name = Console.ReadLine();
+        DateTime currentDate = DateTime.Now;
+        Console.WriteLine($"\n Привет {name}, сейчас {currentDate:d} a");
+        Console.Write($"\n Нажмите любую клавишу для завершения...");
+        Console.ReadKey(true);
+    }
+}
